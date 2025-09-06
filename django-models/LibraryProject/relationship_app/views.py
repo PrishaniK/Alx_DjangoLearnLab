3e90be1feedfae992760f2aa3e9da8a556d1bd57
PrishaniK <prishanik135@gmail>
2025-09-06 21:33:30 +0200

    Refactor auth views to function-based for checker compatibility

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Book
-from .models import Library 
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render, redirect
+from .models import Library
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.views import View
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Function-based view to list all books
 def list_books(request):
@@ -19,24 +17,14 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-# Registration view
-class RegisterView(View):
-    def get(self, request):
-        form = UserCreationForm()
-        return render(request, 'relationship_app/register.html', {'form': form})
-
-    def post(self, request):
+# Function-based register view (required by checker)
+def register(request):
+    if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log the user in after registration
+            login(request, user)
             return redirect('list_books')
-        return render(request, 'relationship_app/register.html', {'form': form})
-
-# Login view using Django's built-in view
-class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
-
-# Logout view using Django's built-in view
-class CustomLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
