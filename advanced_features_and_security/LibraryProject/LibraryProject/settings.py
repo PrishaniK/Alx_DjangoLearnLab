a77fb65d470b304488e2e80a1c2c9af574e4c148
PrishaniK <prishanik135@gmail>
2025-09-13 15:49:49 +0200

    Implement HTTPS settings, HSTS, secure cookies, and headers

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -158,3 +158,4 @@ SECURE_SSL_REDIRECT = True  # Enforces HTTPS
 SECURE_HSTS_SECONDS = 31536000  # 1 year in seconds
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Apply HSTS to all subdomains
 SECURE_HSTS_PRELOAD = True  # Allow preload list inclusion
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
